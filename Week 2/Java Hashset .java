91448ddd281256fe27730f97ccdd131c85952994
Akushwanth <reddyakushwanth@gmail.com>
2026-08-24 17:53:03 +0530

    Java Hashset .java

---
+++ Week 2/Java Hashset .java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        String[] pair_left = new String[t];
+        String[] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = in.next();
+            pair_right[i] = in.next();
+        }
+        
+        HashSet<String> set = new HashSet<>();
+        
+        for (int i = 0; i < t; i++) {
+            set.add(pair_left[i] + " " + pair_right[i]);
+            System.out.println(set.size());
+        }
+    }
+}

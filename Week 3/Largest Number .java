da12085ad0ca6a85efdecfd36bdcaf4d1b9b4af6
Akushwanth <reddyakushwanth@gmail.com>
2026-08-24 18:09:14 +0530

    Largest Number .java

---
+++ Week 3/Largest Number .java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String largestNumber(int[] nums) {
+        // Convert to strings
+        String[] arr = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = String.valueOf(nums[i]);
+        }
+        
+        // Custom sort
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+        
+        // Edge case: all zeros
+        if (arr[0].equals("0")) return "0";
+        
+        // Build result
+        StringBuilder sb = new StringBuilder();
+        for (String s : arr) {
+            sb.append(s);
+        }
+        
+        return sb.toString();
+    }
+}
